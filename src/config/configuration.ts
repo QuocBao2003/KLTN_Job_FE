49aa238de84aa75c239d858f6ac4a208ff2475e4
QuocBao2003 <votranquocbao2806@gmail.com>
2025-12-11 15:50:34 +0700

    update_redirectUrlGG

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 const OAuthConfig = {
   clientId:
     "796960779443-dagi97jtla96l473cfe41o8ma19dokaq.apps.googleusercontent.com",
-  redirectUri: "http://localhost:3000/authenticate",
+  redirectUri: "https://topjob.io.vn/authenticate",
   authUri: "https://accounts.google.com/o/oauth2/v2/auth",
 };
 export default OAuthConfig;
